4f4433124405104656846864b536e6d97fd84200
foway <foway0@gmail.com>
2020-03-05 00:40:17 +0900

    add sequelize helper create & find

--- src/helper/sequelize.ts
+++ src/helper/sequelize.ts
@@ -1,4 +1,5 @@
 import { Model, Sequelize } from 'sequelize';
+import { FindAndCountOptions } from 'sequelize';
 
 class SequelizeHelper {
   static sync<M extends Model>(model?: { new (): M } & typeof Model) {
@@ -7,6 +8,20 @@ class SequelizeHelper {
 
   static async authenticate(sequelize?: Sequelize) {
     return sequelize?.authenticate();
+  }
+
+  static create<M extends Model>(
+    model?: { new (): M } & typeof Model,
+    options?: object
+  ) {
+    return model?.create(options);
+  }
+
+  static findAndCountAll<M extends Model>(
+    model?: { new (): M } & typeof Model,
+    options?: FindAndCountOptions
+  ) {
+    return model?.findAndCountAll(options);
   }
 }
 
